c981e193556831fb92a589c3e62f8f461b2c776d
azanniel <leo.azannielttt@gmail.com>
2023-03-20 22:44:11 -0400

    💄 add income and outcome button to form

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -11,6 +11,19 @@ export const Overlay = styled(Dialog.Overlay)`
   background-color: rgba(0, 0, 0, 0.75);
 `
 
+export const CloseButton = styled(Dialog.Close)`
+  position: absolute;
+  top: 1.5rem;
+  right: 1.5rem;
+
+  background: transparent;
+  border: 0;
+
+  line-height: 0;
+  cursor: pointer;
+  color: ${(props) => props.theme['gray-500']};
+`
+
 export const Content = styled(Dialog.Content)`
   min-width: 32rem;
 
@@ -64,15 +77,37 @@ export const Content = styled(Dialog.Content)`
   }
 `
 
-export const CloseButton = styled(Dialog.Close)`
-  position: absolute;
-  top: 1.5rem;
-  right: 1.5rem;
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
 
-  background: transparent;
+  margin-top: 0.5rem;
+`
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme['gray-700']};
+  padding: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
   border: 0;
-
-  line-height: 0;
+  border-radius: 6px;
   cursor: pointer;
-  color: ${(props) => props.theme['gray-500']};
+
+  color: ${(props) => props.theme['gray-300']};
+
+  svg {
+    color: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-300']
+        : props.theme['red-300']};
+  }
 `
