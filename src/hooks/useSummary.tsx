7389a71bc8cfd6ec5340f370d262fd8d4f843ab1
azanniel <leo.azannielttt@gmail.com>
2023-04-07 17:38:24 -0400

    🎨 add use memo hook in use summary

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
 
@@ -6,19 +7,28 @@ export function useSummary() {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc
-    },
-    { income: 0, outcome: 0, total: 0 },
-  )
+  /**
+   * Diferente do memo, o useMemo serve para memorizar variáveis, enquanto que
+   * o memo faz isso com componentes
+   *
+   * Para funções é muito o usado o useCallback
+   */
+
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+        return acc
+      },
+      { income: 0, outcome: 0, total: 0 },
+    )
+  }, [transactions])
 
   return summary
 }
